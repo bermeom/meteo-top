3d2cde666428e3ae9d08aac374b5352dda7c3519
Miguel Angel Bermeo Ayerbe <bermeom0@gmail.com>
2023-02-19 16:27:23 +0100

    Add unit test in tests folder, using Catch2

---
+++ tests/CURLCommunicationChannel_test.cpp
@@ -0,0 +1,22 @@
+#include "CURLCommunicationChannel.hpp"
+
+#include <catch2/catch_all.hpp>
+
+TEST_CASE("Test a GET request", "[unit]") {
+    METEO_TOP::CURLCommunicationChannel communcation_channel(
+        "https://rickandmortyapi.com/api/character/380");
+    communcation_channel.startup_settings();
+    std::string answer = communcation_channel.request_get();
+    const std::string expected_answer =
+        "{\"id\":380,\"name\":\"Unknown "
+        "Rick\",\"status\":\"unknown\",\"species\":\"Human\",\"type\":\"\","
+        "\"gender\":\"Male\",\"origin\":{\"name\":\"unknown\",\"url\":\"\"},"
+        "\"location\":{\"name\":\"unknown\",\"url\":\"\"},\"image\":\"https://"
+        "rickandmortyapi.com/api/character/avatar/"
+        "380.jpeg\",\"episode\":[\"https://rickandmortyapi.com/api/episode/"
+        "22\",\"https://rickandmortyapi.com/api/episode/51\"],\"url\":\"https:/"
+        "/rickandmortyapi.com/api/character/"
+        "380\",\"created\":\"2018-01-10T19:44:22.262Z\"}";
+    std::cout << answer << std::endl;
+    REQUIRE(expected_answer.compare(answer) == 0);
+}
