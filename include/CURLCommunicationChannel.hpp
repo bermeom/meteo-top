38dc2cd62dc478115b92fa128c1d9adff0035289
Miguel Angel Bermeo Ayerbe <bermeom0@gmail.com>
2023-02-19 13:51:11 +0100

    Add CURLCommunicationChannel.cpp to split the header and implementation

--- include/CURLCommunicationChannel.hpp
+++ include/CURLCommunicationChannel.hpp
@@ -16,36 +16,15 @@ class CURLCommunicationChannel {
     const std::string url_api_;
 
    public:
-    CURLCommunicationChannel(const std::string url_api)
-        : url_api_(url_api), curl_(curl_easy_init()), buffer_("") {}
+    // Constructor
+    CURLCommunicationChannel(const std::string url_api);
 
-    void startup_settings() {
-        if (this->curl_) {
-            curl_easy_setopt(this->curl_, CURLOPT_URL, this->url_api_.c_str());
-            curl_easy_setopt(this->curl_, CURLOPT_WRITEFUNCTION,
-                             CURLCommunicationChannel::write_string_callback);
-            curl_easy_setopt(this->curl_, CURLOPT_WRITEDATA, &this->buffer_);
-        }
-    }
+    void startup_settings();
 
     static size_t write_string_callback(void *contents, size_t size,
-                                        size_t nmemb, void *userdata) {
-        static_cast<std::string *>(userdata)->append(
-            static_cast<char *>(contents), size * nmemb);
-        return size * nmemb;
-    }
+                                        size_t nmemb, void *userdata);
 
-    std::string &request_get() {
-        if (this->curl_) {
-            this->res_ = curl_easy_perform(this->curl_);
-            curl_easy_cleanup(this->curl_);
-            if (this->res_ != CURLE_OK) {
-                fprintf(stderr, "Get request failed: %s\n",
-                        curl_easy_strerror(this->res_));
-            }
-        }
-        return this->buffer_;
-    }
+    std::string &request_get();
 };
 
 }  // namespace METEO_TOP
---
+++ src/CURLCommunicationChannel.cpp
@@ -0,0 +1,44 @@
+
+#include "CURLCommunicationChannel.hpp"
+
+#include <curl/curl.h>
+
+#include <iostream>
+#include <string>
+
+namespace METEO_TOP {
+
+CURLCommunicationChannel::CURLCommunicationChannel(const std::string url_api)
+    : url_api_(url_api), curl_(curl_easy_init()), buffer_("") {}
+
+void CURLCommunicationChannel::startup_settings() {
+    if (this->curl_) {
+        curl_easy_setopt(this->curl_, CURLOPT_URL, this->url_api_.c_str());
+        curl_easy_setopt(this->curl_, CURLOPT_WRITEFUNCTION,
+                         CURLCommunicationChannel::write_string_callback);
+        curl_easy_setopt(this->curl_, CURLOPT_WRITEDATA, &this->buffer_);
+    }
+}
+
+size_t CURLCommunicationChannel::write_string_callback(void *contents,
+                                                       size_t size,
+                                                       size_t nmemb,
+                                                       void *userdata) {
+    static_cast<std::string *>(userdata)->append(static_cast<char *>(contents),
+                                                 size * nmemb);
+    return size * nmemb;
+}
+
+std::string &CURLCommunicationChannel::request_get() {
+    if (this->curl_) {
+        this->res_ = curl_easy_perform(this->curl_);
+        curl_easy_cleanup(this->curl_);
+        if (this->res_ != CURLE_OK) {
+            fprintf(stderr, "Get request failed: %s\n",
+                    curl_easy_strerror(this->res_));
+        }
+    }
+    return this->buffer_;
+}
+
+}  // namespace METEO_TOP
